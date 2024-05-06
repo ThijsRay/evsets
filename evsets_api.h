1ad787447bd4df4ca53d340270f488fd37c8193e
Thijs Raymakers <thijs@raymakers.nl>
2024-05-06 12:34:38 +0200

    Fix prototype without void warnings

--- evsets_api.h
+++ evsets_api.h
@@ -4,9 +4,9 @@
 #include "public_structs.h"
 
 int init_evsets(struct config *conf);
-int find_evsets();
-int get_num_evsets();
-Elem* get_evset(int id);
-void close_evsets();
+int find_evsets(void);
+int get_num_evsets(void);
+Elem *get_evset(int id);
+void close_evsets(void);
 
 #endif /* evsets_api_H */
